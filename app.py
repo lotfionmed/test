560e4624c9794710aeb8b0186258d91fc52658f9
lotfionmed <lotfionmed@gmail.com>
2024-12-01 17:38:26 +0100

    Mise à jour pour le déploiement Render

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 from flask import Flask, render_template
 from flask_socketio import SocketIO, emit
+import os
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'votre_clé_secrète'
-socketio = SocketIO(app)
+socketio = SocketIO(app, cors_allowed_origins="*")
 
 @app.route('/')
 def index():
@@ -14,4 +15,5 @@ def handle_message(message):
     emit('message', message, broadcast=True)
 
 if __name__ == '__main__':
-    socketio.run(app, debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    socketio.run(app, host='0.0.0.0', port=port)
